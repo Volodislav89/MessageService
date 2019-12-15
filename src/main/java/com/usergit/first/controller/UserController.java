3dc84539aeb45369f752f4d4b24508b4b4b8944d
“Volodislav89” <shbloodislav@gmail.com>
2019-12-15 21:57:47 +0200

    DownloadFeature

--- src/main/java/com/usergit/first/controller/UserController.java
+++ src/main/java/com/usergit/first/controller/UserController.java
@@ -4,9 +4,16 @@ import com.usergit.first.model.User;
 import com.usergit.first.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.File;
+import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin
@@ -42,6 +49,15 @@ public class UserController {
     @DeleteMapping("/{userId}")
     public Mono<String> deleteUser(@PathVariable String userId) {
         return userRepository.findById(userId).flatMap(user -> userRepository.delete(user)).then(Mono.just("Deleted"));
+    }
+
+    @GetMapping("/download/{fileName:.+}")
+    public Mono<Void> downloadFile(@PathVariable String fileName, ServerHttpResponse response) {
+        ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
+        response.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=upload/" + fileName);
+        response.getHeaders().setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        File file = Paths.get("upload/" + fileName).toFile();
+        return zeroCopyResponse.writeWith(file, 0, file.length());
     }
 }
 
