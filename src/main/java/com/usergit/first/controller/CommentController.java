420a1d211409e8bc002d431dae67a12ccaab166d
“Volodislav89” <shbloodislav@gmail.com>
2019-12-21 11:33:06 +0200

    Comment zipWith Message

--- src/main/java/com/usergit/first/controller/CommentController.java
+++ src/main/java/com/usergit/first/controller/CommentController.java
@@ -3,6 +3,7 @@ package com.usergit.first.controller;
 import com.usergit.first.model.Comment;
 import com.usergit.first.repository.CommentRepository;
 import com.usergit.first.repository.MessageRepository;
+import com.usergit.first.security.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -49,5 +50,35 @@ public class CommentController {
     @DeleteMapping
     public Mono<String> deleteAllComments() {
         return commentRepository.deleteAll().then(Mono.just("All comments deleted"));
+    }
+
+    @PostMapping
+    public Mono newMessageWithNewComment(@RequestBody Message message, @RequestBody Comment comment) {
+        Mono<Message> messageMono = messageRepository.save(message);
+        Mono<Comment> commentMono = commentRepository.save(comment);
+        return messageMono.zipWith(commentMono).flatMap(tuple -> {
+            tuple.getT1().addCommentToList(tuple.getT2());
+            return messageRepository.save(tuple.getT1());
+        });
+    }
+
+    @PostMapping("/{messageId}/{commentId}")
+    public Mono messageWithComment(@PathVariable String messageId, @PathVariable String commentId) {
+        Mono<Message> messageMono = messageRepository.findById(messageId);
+        Mono<Comment> commentMono = commentRepository.findById(commentId);
+        return messageMono.zipWith(commentMono).flatMap(tuple -> {
+            tuple.getT1().addCommentToList(tuple.getT2());
+            return messageRepository.save(tuple.getT1());
+        });
+    }
+
+    @PostMapping("/{messageId}")
+    public Mono messageWithNewComment(@PathVariable String messageId, @RequestBody Comment comment) {
+        Mono<Message> messageMono = messageRepository.findById(messageId);
+        Mono<Comment> commentMono = commentRepository.save(comment);
+        return messageMono.zipWith(commentMono).flatMap(tuple -> {
+            tuple.getT1().addCommentToList(tuple.getT2());
+            return messageRepository.save(tuple.getT1());
+        });
     }
 }
--- src/main/java/com/usergit/first/security/model/Message.java
+++ src/main/java/com/usergit/first/security/model/Message.java
@@ -1,11 +1,15 @@
 package com.usergit.first.security.model;
 
+import com.usergit.first.model.Comment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document
 @Data
@@ -16,9 +20,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Message {
     @Id
     private String id;
-    private  String content;
+    private String content;
+    private List<Comment> comments = new ArrayList<>();
 
     public Message(String content) {
         this.content = content;
+    }
+
+    public List<Comment> addCommentToList(Comment comment) {
+        this.comments.add(comment);
+        return this.comments;
     }
 }
